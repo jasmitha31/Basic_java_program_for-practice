8fd719d82869dc95feb9c1dd3575fc26afcde7be
jasmitha31 <147499600+jasmitha31@users.noreply.github.com>
2024-05-14 11:48:01 +0530

    Update perfect.java

--- perfect.java
+++ perfect.java
@@ -1,29 +1,21 @@
 import java.util.Scanner;
-public class Perfect {
+public class perfectNum{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the no. - ");
         long n=in.nextInt();
-        int i;
-        for(i = 2; i < n; i++) {              
-            if(isNumPerfect(i)) {
-                System.out.println(i + " is a perfect number");
-            }
+       long sum=0;
+       for(int i=1;i<=n/2;i++){
+           if(n%i==0){
+               sum+=i;
+           }
+       }
+       if(n==sum){
+            System.out.println("Perfect");
+        }else{
+            System.out.println("Not Perfect");
         }
-        in.close();
+       in.close();
     }
-    public static boolean isNumPerfect(int i) {
-        boolean isPerfect = false;
-        int sum = 1;
-        int x;
-        for(x = 2; x <= i / 2; x++) {
-            if(i % x == 0) {
-                sum += x;
-            }
-        }
-        if(sum == i) {
-            isPerfect = true;
-        }
-        return isPerfect;
-    }
+    
 }
