63ddc445760250e31a3605ded7d8e8d23dfa8f3c
jasmitha31 <147499600+jasmitha31@users.noreply.github.com>
2024-10-16 09:58:44 +0530

    Create even_odd.java

---
+++ even_odd.java
@@ -0,0 +1,10 @@
+import java.lang.*;	
+public class day1{
+public static void main(String [] args){
+int a=5;
+if(a%2==0){
+	System.out.println("This is a even number");}
+else{
+	System.out.println("This is a odd number");}
+}
+}
