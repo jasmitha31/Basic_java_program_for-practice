44fb204920a649c8cb341ef216b01351ace41340
jasmitha31 <147499600+jasmitha31@users.noreply.github.com>
2024-10-16 09:59:36 +0530

    Create greatest_of_2_num.java

---
+++ greatest_of_2_num.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class day1_3{
+public static void main(String [] args){
+int a=10;
+int b=1;
+if(a>b){
+	System.out.println("a "+a+" is the greatest");}
+else{
+	System.out.println("b "+b+" is the greatest");}
+}
+}
+	
